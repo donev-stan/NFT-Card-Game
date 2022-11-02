d5250cc5b27fb3525aabb20e2e59924a9d3e35ab
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-11-02 18:04:53 +0200

    basic design

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -5,8 +5,14 @@ import { logo, heroImg } from '../assets';
 import styles from '../styles';
 
 const PageHOC = (Component, title, description) => {
+    const navigate = useNavigate();
+
   return (
-    <div>PageHOC</div>
+    <div className={styles.hocContainer}>
+        <div className={styles.hocContentBox}>
+            <img src={logo} alt="logo" className={styles.hocLogo} onClick={() => navigate('/')} />
+        </div>
+    </div>
   )
 }
 
