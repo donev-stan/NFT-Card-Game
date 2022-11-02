a5b0b20b726b7c9b4732b8087cd4b86ccedbbfcd
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-11-02 18:01:55 +0200

    initial component creation

---
+++ client/src/components/PageHOC.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { logo, heroImg } from '../assets';
+import styles from '../styles';
+
+const PageHOC = (Component, title, description) => {
+  return (
+    <div>PageHOC</div>
+  )
+}
+
+export default PageHOC
